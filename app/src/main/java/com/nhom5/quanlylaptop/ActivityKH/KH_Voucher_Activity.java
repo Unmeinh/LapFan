81893af70104b7bfc1c52a0fd38761b8340b22d8
Unmeinh <khongphaihoanglinh@gmail.com>
2022-11-16 22:03:25 +0700

    Update 22:00 16/11/22

--- app/src/main/java/com/nhom5/quanlylaptop/ActivityKH/KH_Voucher_Activity.java
+++ app/src/main/java/com/nhom5/quanlylaptop/ActivityKH/KH_Voucher_Activity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -27,15 +29,24 @@ public class KH_Voucher_Activity extends AppCompatActivity {
     QL_Voucher_Adapter ql_voucher_adapter;
     String TAG = "QL_Voucher_Fragment_____";
     Context context = this;
+    ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kh_voucher);
         recyclerView = findViewById(R.id.recyclerView_Voucher_KH);
+        backButton = findViewById(R.id.imageButton_Back);
         voucherDAO = new VoucherDAO(context);
 
         listVou = voucherDAO.selectVoucher(null, null, null, null);
         setUpRecyclerView(context);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void setUpRecyclerView(Context context) {
